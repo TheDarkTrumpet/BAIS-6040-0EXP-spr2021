faa9a20f24270c355d1e03635508a540c1587c7f
David Thole <david@thedarktrumpet.com>
2021-03-24 17:51:23 -0500

    Adding flask application, base

---
+++ Notebooks/Phase 3/10.01.01-Flask/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask.templating import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/report')
+def report():
+    return render_template('report.html')
+
+if __name__ == '__main__':
+    app.run()
