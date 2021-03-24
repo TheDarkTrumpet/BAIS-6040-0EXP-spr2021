dcf91b5635da57db2e4a7955f1adf45c5cd655e9
David Thole <david@thedarktrumpet.com>
2021-03-24 18:05:20 -0500

    Adding a very simple index page, and linking to one template so far, with a reference

--- Notebooks/Phase 3/10.01.01-Flask/app.py
+++ Notebooks/Phase 3/10.01.01-Flask/app.py
@@ -3,15 +3,20 @@ from flask.templating import render_template
 
 app = Flask(__name__)
 
+allReports = [
+    ("/report", "Sample Report")
+]
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', reports=allReports)
 
 
 @app.route('/report')
 def report():
     return render_template('report.html')
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
