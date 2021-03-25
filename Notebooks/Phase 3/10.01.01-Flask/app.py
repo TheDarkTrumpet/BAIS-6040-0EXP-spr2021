0d4a8d68c94015c4756bc711331627236596a2ce
David Thole <david@thedarktrumpet.com>
2021-03-25 05:37:32 -0500

    Finishing flask example

--- Notebooks/Phase 3/10.01.01-Flask/app.py
+++ Notebooks/Phase 3/10.01.01-Flask/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask.templating import render_template
+from lib.plot import getPiePlot, getChartPlot
 
 app = Flask(__name__)
 
 allReports = [
-    ("/report", "Sample Report")
+    ("/pieplot", "Pie Rates in Mexico (Pie Plot)"),
+    ("/barchart", "Pie Rates in Mexico (Bar Chart Plot)")
 ]
+
+
+def plot(data):
+    return render_template('report.html', report=data)
 
 
 @app.route('/')
@@ -13,9 +19,16 @@ def index():
     return render_template('index.html', reports=allReports)
 
 
-@app.route('/report')
-def report():
-    return render_template('report.html')
+@app.route('/pieplot')
+def pieplot():
+    chart = getPiePlot()
+    return plot(chart)
+
+
+@app.route('/barchart')
+def piechart():
+    chart = getChartPlot()
+    return plot(chart)
 
 
 if __name__ == '__main__':
